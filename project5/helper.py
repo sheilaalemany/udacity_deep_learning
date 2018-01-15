c88c3e97c8d26c1bcee12e88390346f9b43163de
System Administrator <salem010@fiu.edu>
2018-01-15 18:47:20 -0500

    updated project5

--- project5/helper.py
+++ project5/helper.py
@@ -210,9 +210,9 @@ class Dataset(object):
         current_index = 0
         while current_index + batch_size <= self.shape[0]:
             data_batch = get_batch(
-                self.data_files[current_index:current_index + batch_size],
+                image_file = self.data_files[current_index:current_index + batch_size],
                 *self.shape[1:3],
-                self.image_mode)
+                mode = self.image_mode)
 
             current_index += batch_size
 
